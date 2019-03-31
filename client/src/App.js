755734be503c01f73db9c2651d623ba11ae381f9
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2019-03-31 14:11:39 +0100

    Update header

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <h1>Fib Calculator</h1>
+            <h1>Fib Calculator in Kubernetes!</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
